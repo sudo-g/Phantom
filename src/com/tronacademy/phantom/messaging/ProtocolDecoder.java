a1936690494a89d792721d176a5a849d03534c80
George Xian <george_xian@live.com>
2015-05-16 15:21:11 +1200

    Changed ProtocolDecoder to interface

--- src/com/tronacademy/phantom/messaging/ProtocolDecoder.java
+++ src/com/tronacademy/phantom/messaging/ProtocolDecoder.java
@@ -1,9 +1,4 @@
- package com.tronacademy.phantom.messaging;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-import com.tronacademy.phantom.comm.CommManager;
+package com.tronacademy.phantom.messaging;
 
 import android.graphics.Bitmap;
 
@@ -25,7 +20,7 @@ import android.graphics.Bitmap;
  * @since 2015-05-03
  * 
  */
-public abstract class ProtocolDecoder {
+public interface ProtocolDecoder {
 	
 	public final static int DEFAULT_POLLING_INT = 100;
 	
@@ -39,116 +34,25 @@ public abstract class ProtocolDecoder {
 	 * </ul>
 	 */
 	public enum ErrorType {SYS_DECODE_ERR, NET_BUFFER_FULL};
-	private volatile boolean mRunning = false;
-	private final Thread decodingProcess = new Thread() {
-		@Override
-		public void run() {
-			while(mRunning) {
-				InputStream in = mCommManager.getInputStream();
-				try {
-					if (in.available() > 0) {
-						decode((byte) in.read());
-						in.close();
-					} else {
-						in.close();   // close stream ASAP instead of sleeping first
-						Thread.sleep(mPollingInterval);
-					}
-				} catch (IOException e) {
-					if (mListener != null) {
-						mListener.onError(ErrorType.SYS_DECODE_ERR, 0);
-					}
-				} catch (InterruptedException e) {
-					// set the flag to match actual state
-					mRunning = false;
-				}
-			}
-		}
-	};
-	
-	private volatile int mPollingInterval = DEFAULT_POLLING_INT;
-	protected ProtocolDecodeListener mListener;
-	protected CommManager mCommManager;
 	
 	/**
-	 * @param commManager Manager for the communication hardware to use.
+	 * @return The string name of this protocol decoder type.
 	 */
-	public ProtocolDecoder(CommManager commManager) {
-		mCommManager = commManager;
-	}
-	
-	/**
-	 * Changed the hardware used to perform communication.
-	 * 
-	 * @return Flag indicating whether operation succeeded.
-	 */
-	public boolean changeCommManager(CommManager commManager) {
-		if (!mRunning) {
-			mCommManager = commManager;
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	/**
-	 * Start decoding process with bound communication manager.
-	 * 
-	 * @return Flag indicating whether decoder has started.
-	 */
-	public boolean start() {
-		if (mCommManager != null) {
-			mRunning = true;
-			decodingProcess.start();
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	/**
-	 * Stop decoding process.
-	 */
-	public void stop() {
-		mRunning = false;
-	}
-	
-	/**
-	 * @return Flag indicating whether decoder is running.
-	 */
-	public boolean isRunning() {
-		return mRunning;
-	}
-	
-	/**
-	 * Set how often the input buffer is checked. 
-	 * 
-	 * @param millis Interval in milliseconds.
-	 */
-	public synchronized void setPollingInterval(int millis) {
-		mPollingInterval = millis;
-	}
+	public String getName();
 	
 	/**
 	 * Implementation for how each byte is interpreted.
 	 * 
 	 * @param read Byte read from stream.
-	 * @throws IOException if error encountered reading from stream.
 	 */
-	protected abstract void decode(byte read);
-	
-	/**
-	 * @return The string name of this protocol decoder type.
-	 */
-	public abstract String getName();
+	public void decodeByte(byte read);
 	
 	/**
 	 * Attach custom hook to ProtocolDecoder events.
 	 * 
 	 * @param listener User defined event callbacks for this decoder.
 	 */
-	public void setOnProtocolDecodeListener(ProtocolDecodeListener listener) {
-		mListener = listener;
-	}
+	public void setOnProtocolDecodeListener(ProtocolDecodeListener listener);
 	
 	public interface ProtocolDecodeListener {
 		/**
